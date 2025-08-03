8da57ed022d4777aecc6e74cc1a5c187d8c779af
Fathiralfariz <alfarizifathir029@gmail.com>
2025-08-03 16:30:57 +0700

    UPDATE DATABASE 3

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php 
 $host = 'localhost'; 
-$user = 'root'; 
-$pass = ''; 
-$db_name = 'db_pinjambuku'; 
+$user = 'rootif0_39623030'; 
+$pass = 'i6HmOUckQwZV'; 
+$db_name = 'if0_39623030_XXX'; 
 $conn = new mysqli($host, $user, $pass, $db_name); 
 if ($conn->connect_error) { 
 die('Koneksi gagal: ' . $conn->connect_error); 
--- index.php
+++ index.php
@@ -4,12 +4,12 @@ if (!isset($_SESSION['petugas'])) {
 header("Location: pages/login.php"); 
 exit; 
 } 
-?> 
+?>
 <h2>Selamat Datang, <?= htmlspecialchars($_SESSION['petugas']['nama']) 
-?></h2> 
-<ul> 
-<li><a href="pages/anggota/">Manajemen Anggota</a></li> 
-<li><a href="pages/buku/">Manajemen Buku</a></li> 
-<li><a href="pages/peminjaman/">Peminjaman Buku</a></li> 
-<li><a href="logout.php">Logout</a></li> 
+?></h2>
+<ul>
+    <li><a href="pages/anggota/">Manajemen Anggota</a></li>
+    <li><a href="pages/buku/">Manajemen Buku</a></li>
+    <li><a href="pages/peminjaman/">Peminjaman Buku</a></li>
+    <li><a href="logout.php">Logout</a></li>
 </ul>
